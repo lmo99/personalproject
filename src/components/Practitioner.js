03bd9efd571ac093d7e800728797f1673a1467f5
Lotanna <lotannaokaro@gmail.com>
2020-12-14 19:17:12 +0000

    cant remember if ive already committed but profiles showing, now need to add cards

--- src/components/Practitioner.js
+++ src/components/Practitioner.js
@@ -4,6 +4,14 @@ const Practitioner = (props) => {
     return(
         <div>
             <div className="profile__name">{props.profile.name}</div>
+            <div className="profile__bio">{props.profile.bio}</div>
+            <div className="profile__speciality">{props.profile.profileInfo.speciality}</div>
+            <div className="profile__typeOfSession">{props.profile.profileInfo.typeOfSession}</div>
+            <div className="profile__gender">{props.profile.profileInfo.gender}</div>
+            <div className="profile__ethnicity">{props.profile.profileInfo.ethnicity}</div>
+
+
+
         </div>
     )
     
--- src/components/PractitionerList.js
+++ src/components/PractitionerList.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import Practitioner from './Practitioner';
 
 const PractitionerList = (props) => {
+
     return(
         <div>
-            {props.data.map(profile =><Practitioner key={profile.name}
-            profile={profile}/>)}
+            {props.data.map(profile =><Practitioner key={profile.id}
+            profile={profile}/>) }
         </div>
+
+        
     )
 }
 export default PractitionerList;

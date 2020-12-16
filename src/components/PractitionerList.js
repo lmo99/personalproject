3652b9aea61ba05745a4c56efb3855006564d30c
Lotanna <lotannaokaro@gmail.com>
2020-12-16 00:04:04 +0000

    bootstrap cards added

--- src/components/PractitionerList.js
+++ src/components/PractitionerList.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap';
 import Practitioner from './Practitioner';
 
 const PractitionerList = (props) => {
-
+console.log(props.data)
     return(
         <div>
-            {props.data.map(profile =><Practitioner key={profile.id}
-            profile={profile}/>) }
+            {props.data.map(profile =><Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>
+                        {profile.name}
+                    </Card.Title>
+                    <Card.Text>
+                        {profile.bio}
+                    </Card.Text>
+                </Card.Body>
+            </Card>)}
+            
         </div>
 
         

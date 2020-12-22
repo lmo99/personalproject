485c8c3d9c9b4378fc196c0a7901f5df2dee9b6c
Lotanna <lotannaokaro@gmail.com>
2020-12-22 22:29:31 +0000

    added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import '../App.css';
+import '../assets/Nav.css';
 
 const Header = ({practitionerList}) => {
     return (
         <div>
             <br></br>
             <h1>Find a therapist that's right for you... </h1>
-            <Link to="/"> Home </Link>
-            <Link to="/savedprofiles" className="savedProfiles"> 
-                Saved Profiles ({practitionerList.length}) 
-            </Link>
+            <div className="nav">
+                <Link to="/"> Home </Link>
+                <Link to="/savedprofiles" className="savedProfiles"> 
+                    Saved Profiles ({practitionerList.length}) 
+                </Link>
+            </div>
         </div>
     );
 }
